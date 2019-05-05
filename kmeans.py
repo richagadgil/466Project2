7cc57b54fedadf53d4be9c43be73cef58ae223ba
RichaGadgil <gadgilricha@gmail.com>
2019-05-05 16:42:48 -0700

    kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,9 @@
 import sys
 import numpy
 import re
-
+import random
+import numpy as np
+from sklearn.cluster import KMeans
 
 
 class Record:
@@ -15,6 +17,16 @@ class Record:
     text = None
 
 
+    def __init__(self):
+        pass
+    
+    def add_cid(self, cid):
+        self.cid = cid
+    
+    def add_text(self, text):
+        self.text = text
+
+
 def main():
     args = sys.argv[1:]
 
@@ -24,28 +36,41 @@ def main():
     
     filename = args[0]
     unique_words = []
+    records = [] #total records
 
 
     with open(filename, 'r') as f:
         D = []
-        counter = 1
+        counter = 0
         for line in f:
             if counter == 3:
                 break
-            words = line.split('\t')
-            filtered = [word.strip() for word in words]
+            words = line.split('\t') #split words along tabs
+            print(words)
+            filtered = [word.strip() for word in words] #strip whitespace 
             filtered[:] = [x for x in filtered if x != '']
-            text = re.sub('[^A-Za-z0-9 ]+', '', words[14]).lower().split()
-            get_features(text)
+            text = re.sub('[^A-Za-z0-9 ]+', '', words[14]).lower().split() #remove punctuation/special chars from words
+            get_features(text) 
             unique_words += (text)
             counter += 1
+
+            if(counter > 1): #add all records to record list EXCEPT FOR HEADER
+                record = Record() #create new Record
+                record.add_cid(int(words[2])) #add CID & Text
+                record.add_text(words[14])
+
+                records.append(record) # append to record list 
+
+            
+
+
+            
+    print(len(records))
+    
    
     unique_words = list(set(unique_words))
 
     print(len(unique_words))
-
-
-
 
 
 def get_features(words):
@@ -69,5 +94,7 @@ def computeTF(bag_of_words, words):
         tf_values[key] = tf
 	
     return tf_values
+
+
 if __name__ == '__main__':
     main()
