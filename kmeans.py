83ed390504af0a1342c10aa7385561bc1983f742
RichaGadgil <gadgilricha@gmail.com>
2019-05-08 17:41:21 -0700

    more feature extraction

--- kmeans.py
+++ kmeans.py
@@ -6,6 +6,10 @@ import random
 from sklearn.cluster import KMeans
 from nltk.corpus import stopwords 
 from nltk.corpus import wordnet as wn
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+
 class Record:
 
     cid = None
@@ -45,7 +49,7 @@ def main():
     features = []
 
     c_names = {}
-
+    all_c_names = {}
     with open(filename, 'r') as f:
         num_records = 0
         for line in f:
@@ -58,6 +62,8 @@ def main():
             r.add_c_name(words[3])
             r.add_text(words[14])
             records.append(r)
+            if words[3] not in all_c_names:
+                all_c_names[words[3]] = 1
             #filtered = [word.strip() for word in words]
             #filtered[:] = [x for x in filtered if x != '']
             #feature = get_features(words[14])
@@ -74,7 +80,7 @@ def main():
             #if(num_records == 3000):
             #    break  
     #print(data['data'])
-    
+
     test_records = []
     for i in range(4000):
         record = random.choice(records)
@@ -98,8 +104,10 @@ def main():
 
 
     print("My Kmeans labels:")
-    clusters = my_kmeans(np.array(test_records), len(c_names), 0.01)
-    contingency_table(c_names, clusters)
+    for i in range(5, 16):
+        print("K:", i)
+        clusters = my_kmeans(np.array(test_records), i, 0.01)
+        contingency_table(c_names, clusters)
 
     #print("Sklearn kmeans labels:")
     #get_scikit_kmeans_centroids(3,np.array(vectors),0.01)
@@ -108,12 +116,13 @@ def main():
 
 
 def contingency_table(labels, clusters):
-    print(labels)
+    #print(labels)
  
     purity_total_rows = sum(labels.values())
     purity_max_sum = 0
 
     rows = {}
+    sum_f1 = 0
 
 
     for clusterNo in range(0, len(clusters)):
@@ -132,24 +141,43 @@ def contingency_table(labels, clusters):
         purity_max_sum += max(row) #Add maximum label value present in cluster
         f1 = (2 * precision * recall) / (precision + recall)
 
-        print(clusterNo , " ", row)
-        print("Precision of Cluster", clusterNo, "=", precision) 
-        print("Recall of Cluster", clusterNo, "=", recall) 
-        print("F1 Score of Cluster", clusterNo, "=", f1, "\n\n\n") 
-
+       # print(clusterNo , " ", row)
+       # print("Precision of Cluster", clusterNo, "=", precision) 
+       # print("Recall of Cluster", clusterNo, "=", recall) 
+       # print("F1 Score of Cluster", clusterNo, "=", f1, "\n\n\n") 
+        sum_f1 += f1
     
 
+    average_f1 = sum_f1/len(clusters)
+
+    
+    print("Average F1", average_f1) #F1 Avg Calculation
     print("Purity", purity_max_sum/purity_total_rows) #Purity Calculation
 
     
 def pre_process(text):
-    words = nltk.word_tokenize(text)
-    tags = nltk.pos_tag(words)
+    #words = nltk.word_tokenize(text)
+    #tags = nltk.pos_tag(words)
     lemmatizer = nltk.stem.WordNetLemmatizer()
-    target_tags = ["JJ", "JJR", "JJS", "NN", "NNP", "NNPS", "NNS", "PRP", "PRP$", "RB", "RBR", "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
-    filtered_words = [tag for tag in tags if tag[1] in target_tags]
-    filtered_words = [word for word in filtered_words if word[0].lower() not in stopwords.words('english')]            
-    filtered_words = [lemmatizer.lemmatize(word[0]) for word in filtered_words]
+    #target_tags = ["JJ", "JJR", "JJS", "NN", "NNP", "NNPS", "NNS", "PRP", "PRP$", "RB", "RBR", "RBS", "VB", "VBD", "VBG", "VBN", "VBP", "VBZ"]
+    #target_tags = ["NN", "NNP", "NNPS", "NNS"]
+    #filtered_words = [tag for tag in tags if tag[1] in target_tags]
+    #filtered_words = [word for word in filtered_words if word[0].lower() not in stopwords.words('english')]            
+    #filtered_words = [lemmatizer.lemmatize(word[0]) for word in filtered_words]
+    #print(filtered_words)
+    
+
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = text.lower()
+    text=re.sub("(\\d|\\W)+"," ",text)
+    words = nltk.word_tokenize(text)
+    filtered_words = [word for word in words if word not in stopwords.words('english')]
+    filtered_words = [lemmatizer.lemmatize(word) for word in filtered_words]
+
+
+
+
+
     return filtered_words
 
 def get_wordnet_pos(treebank_tag):
@@ -168,23 +196,27 @@ def get_wordnet_pos(treebank_tag):
 def get_features(text):
     filtered_words = pre_process(text)  
     features = {}
-    if len(filtered_words) < 3:
-        return {}
-    elif len(filtered_words) == 3:
-        just_words = [word[0] for word in filtered_words]
-        featureName = ' '.join(just_words)
-        if featureName in features:
-            features[featureName] += 100
-        else:
-            features[featureName] = 100
-    else:
-        for window in range(len(filtered_words)-2):
-            just_words = [word[0] for word in filtered_words]
-            featureName = ' '.join(just_words[window:window+3])
-            if featureName in features:
-                features[featureName] += 100
-            else:
-                features[featureName] = 100
+    # if len(filtered_words) < 2:
+    #     return {}
+    # elif len(filtered_words) == 2:
+    #     featureName = ' '.join(filtered_words)
+    #     if featureName in features:
+    #         features[featureName] += 100
+    #     else:
+    #         features[featureName] = 100
+    # else:
+    #       for window in range(len(filtered_words)-1):
+    #         featureName = ' '.join(filtered_words[window:window+2])
+    #         if featureName in features:
+    #             features[featureName] += 100
+    #         else:
+    #             features[featureName] = 100
+    for word in filtered_words:
+       if word in features:
+           features[word] += 100
+       else:
+           features[word] = 100
+
     return features
 def get_scikit_kmeans_centroids(num_clusters, vectors, tolerance):
     print(list(KMeans(
@@ -206,7 +238,7 @@ def _closest_cluster_index(feature_x_j, centroids):
         #euclidean_distance = np.linalg.norm(feature_x_j - centroid_i)
         #print("hi\n")
         #print(type(centroid_i))
-        euclidean_distance = np.linalg.norm(feature_x_j.vector - centroid_i)
+        euclidean_distance = np.linalg.norm(feature_x_j.vector - centroid_i) ** 2
        
 
         if euclidean_distance < closest_dist:
@@ -254,7 +286,7 @@ def my_kmeans(Data, k, e=0.001):
         # Check if within error
         for centroid, last_centroid in zip(centroids, last_centroids):
             if len(last_centroids) > 0 and np.sum((centroid - last_centroid)**2) <= e:
-               #print(labels)
+                #print(labels)
                 return clusters  # Optimal clustering achieved.
 
         # Save current to t-1
